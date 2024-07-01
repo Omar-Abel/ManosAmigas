c7a2fec4d9bfe03a9e18035d6876b619fde8c4a7
Omar-Abel <omarlora14@gmail.com>
2024-07-01 09:43:04 -0400

    MAN-20 Configuracion inicial

--- ManosAmigas_Back/Program.cs
+++ ManosAmigas_Back/Program.cs
@@ -1,6 +1,10 @@
 
 using ManosAmigas_Back.Sources.Application;
 using ManosAmigas_Back.Sources.Persistence;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityMo  del.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 namespace ManosAmigas_Back
 {
@@ -14,15 +18,64 @@ namespace ManosAmigas_Back
             var services = builder.Services;
 
             var confi = builder.Configuration;
+            var JWTSetting = builder.Configuration.GetSection("JWTSetting");
 
             services.AddPersistenceInfrastructure(confi);
 
             services.AddApplicationLayer(confi);
 
+
+            builder.Services.AddAuthentication(opt => {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(opt => {
+                opt.SaveToken = true;
+                opt.RequireHttpsMetadata = false;
+                opt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidAudience = JWTSetting["ValidAudience"],
+                    ValidIssuer = JWTSetting["ValidIssuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JWTSetting.GetSection("securityKey").Value!))
+
+
+                };
+            });
+
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c => {
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization Example : 'Bearer eyeleieieekeieieie",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement(){
+        {
+            new OpenApiSecurityScheme{
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "outh2",
+                Name="Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
+
+            });
 
             var app = builder.Build();
 
