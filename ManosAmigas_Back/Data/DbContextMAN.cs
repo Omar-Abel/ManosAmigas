803e788d2e76d0493c2f6071d1d2d4671229d236
Omar-Abel <omarlora14@gmail.com>
2024-07-09 09:21:42 -0400

    MAN-22 Carpetas estructuradas

---
+++ ManosAmigas_Back/Data/DbContextMAN.cs
@@ -0,0 +1,14 @@
+﻿using ManosAmigas_Back.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ManosAmigas_Back.Data
+{
+    public class DbContextMAN : IdentityDbContext<Users>
+    {
+        public DbContextMAN(DbContextOptions<DbContextMAN> options) : base(options)
+        {
+
+        }
+    }
+}
