fc517f221f15b2f26f57bf7934d903a6f8930985
Omar-Abel <omarlora14@gmail.com>
2024-07-14 17:33:27 -0400

    change the context connection string

--- ManosAmigas_Back/Data/ManosAmigasDbContext.cs
+++ ManosAmigas_Back/Data/ManosAmigasDbContext.cs
@@ -2,30 +2,47 @@
 using System.Collections.Generic;
 using ManosAmigas_Back.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
-namespace ManosAmigas_Back.Data;
-
-public partial class ManosAmigasDbContext : DbContext
+namespace ManosAmigas_Back.Data
 {
-    public ManosAmigasDbContext()
+    public partial class ManosAmigasDbContext : DbContext
     {
+        public ManosAmigasDbContext()
+        {
+        }
+
+        public ManosAmigasDbContext(DbContextOptions<ManosAmigasDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // Cargar la configuración desde el archivo appsettings.json
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                // Obtener el connection string
+                var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+                // Configurar el contexto para usar SQL Server con el connection string
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
-
-    public ManosAmigasDbContext(DbContextOptions<ManosAmigasDbContext> options)
-        : base(options)
-    {
-    }
-
-    public virtual DbSet<User> Users { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=OMAR-LAPTOPWORK\\SQLEXPRESS;Database=ManosAmigasDB;Trusted_Connection=True;MultipleActiveResultSets=True;Encrypt=False;");
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        OnModelCreatingPartial(modelBuilder);
-    }
-
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
