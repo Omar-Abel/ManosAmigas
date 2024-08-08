c7f32e4596b342ee569cd01b4de7a3c13fff6047
Omar-Abel <omarlora14@gmail.com>
2024-08-08 15:42:42 -0400

    arreglos

--- ManosAmigas_Front/src/app/app.routes.ts
+++ ManosAmigas_Front/src/app/app.routes.ts
@@ -9,12 +9,11 @@ import {CreateActivitiesComponent} from '../app/create-activities/create-activit
 
 const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
-  { path: 'activities', component: ActivitiesComponent, canActivate: [AuthGuard] },
   { path: 'register', component: RegisterComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent, canActivate: [AuthGuard] },
   { path: 'contact', component: ContactComponent, canActivate: [AuthGuard] },
-  { path: 'listActivities', component: ActivityListComponent, canActivate [AuthGuard] },
-  { path: 'createActivities', component: CreateActivitiesComponent, canActivate [AuthGuard] },
-  { path: '**', redirectTo: '', pathMatch: 'full' }
+  { path: 'listActivities', component: ActivityListComponent, canActivate: [AuthGuard] },
+  { path: 'createActivities', component: CreateActivitiesComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: '', pathMatch: 'full' }]
 
 export { routes };
